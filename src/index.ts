6f4dbe15fcf365386fe7b63d2a7cde7079586b0e
Filippo Quattrocchi <filippo.quattrocchi23@outlook.it>
2024-08-03 17:38:18 +0900

    Feat: Auth required

--- src/index.ts
+++ src/index.ts
@@ -1,92 +1,134 @@
 // src/index.js {path: './.env.local'}
-import express, { Express, request, Request, Response, Router } from "express";
+import express, {
+	Express,
+	request,
+	Request,
+	Response,
+	Router,
+	NextFunction,
+} from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import { handleGETOneEvent, handlePostOneEvent, handlePutOneEvent, handleDeleteOneEvent, handleGetAllEventsInfo } from "./event/event.controller"
+import {
+	handleGETOneEvent,
+	handlePostOneEvent,
+	handlePutOneEvent,
+	handleDeleteOneEvent,
+	handleGetAllEventsInfo,
+} from "./event/event.controller";
 import bcrypt from "bcrypt";
-const saltRounds = 8;
+import jwt, { Secret, JwtPayload } from "jsonwebtoken";
 import { database } from "./knex";
 import { loginRequest, registerRequest } from "./global";
 
-dotenv.config({ path: './.env.local' });
+dotenv.config({ path: "./.env.local" });
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
+const saltRounds = 8;
 const corsOptions = {
-  origin: process.env.VITE_ORIGIN
-}
+	origin: process.env.VITE_ORIGIN,
+};
 
-app.use(express.json())
+app.use(express.json());
 app.use(cors(corsOptions));
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE");
-  next();
+	res.header("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE");
+	next();
 });
 
+export const SECRET_KEY: Secret = "your-secret-key-here";
+
+export interface CustomRequest extends Request {
+	token: string | JwtPayload;
+}
+
+//Auth middllware
+const auth = async (req: Request, res: Response, next: NextFunction) => {
+	try {
+		const token = req.header("Authorization");
+
+		if (!token) {
+			throw new Error();
+		}
+
+		const decoded = jwt.verify(token, SECRET_KEY);
+		(req as CustomRequest).token = decoded;
+
+		next();
+	} catch (err) {
+		res.status(401).send("Please authenticate");
+	}
+};
+
 app.post("/login", async (req: Request, res: Response) => {
-  const userCredential: loginRequest = req.body;
-	const hashPassword = await bcrypt.hash(userCredential.password, saltRounds);
-  const result = await database("user")
-		.select(['id','password'])
+	const userCredential: loginRequest = req.body;
+	const result = await database("user")
+		.select(["id", "password"])
 		.where("username", userCredential.username)
 		.first();
 
-  if(result) {
-    console.log(result, hashPassword, bcrypt.compareSync(userCredential.password, result.password));
-    if(bcrypt.compareSync(userCredential.password, result.password)){
-      res.send("Password giusta!");
-    }
-    else {
-      res.send("Oh no");
-    }
-  }
-  else res.sendStatus(500);
-	
+	if (result) {
+		const username = result.username;
+		const id = result.id;
+		if (bcrypt.compareSync(userCredential.password, result.password)) {
+			const token = jwt.sign(
+				{ id: result.id?.toString(), username: result.username },
+				SECRET_KEY,
+				{
+					expiresIn: "2 days",
+				}
+			);
+
+			const cookie = { username: { id, username }, token: token };
+			res.send(cookie);
+		} else {
+			res.sendStatus(401);
+		}
+	} else res.sendStatus(500);
 });
 
 app.post("/register", async (req: Request, res: Response) => {
 	const userCredential: registerRequest = req.body;
-  try {
-    const hashPassword = await bcrypt.hash(userCredential.password, saltRounds);
-    const newUserData = {
-      username: userCredential.username,
-      password: hashPassword,
-      email: userCredential.email,
-    }
-    const result = await database("user").insert(newUserData, ["id"]);
-    res.json(result);
-  } catch (error) {
-    res.sendStatus(500);
-  }
-	
-
+	try {
+		const hashPassword = await bcrypt.hash(userCredential.password, saltRounds);
+		const newUserData = {
+			username: userCredential.username,
+			password: hashPassword,
+			email: userCredential.email,
+		};
+		const result = await database("user").insert(newUserData, ["id"]);
+		res.json(result);
+	} catch (error) {
+		res.sendStatus(500);
+	}
 });
 
-app.get("/event/:id", async (req: Request, res: Response) => {
-  const result = await handleGETOneEvent(req, res)
-  res.json(result);
+app.get("/event/:id", auth, async (req: Request, res: Response) => {
+	const result = await handleGETOneEvent(req, res);
+	res.json(result);
 });
 
-app.post("/event", async (req: Request, res: Response) => {
-  const result = await handlePostOneEvent(req, res)
-  res.json(result);
+app.post("/event", auth, async (req: Request, res: Response) => {
+	const result = await handlePostOneEvent(req, res);
+	res.json(result);
 });
 
-app.put("/event", (req: Request, res: Response) => {
-  const result = handlePutOneEvent(req, res)
-  res.json(result)
+app.put("/event", auth, (req: Request, res: Response) => {
+	const result = handlePutOneEvent(req, res);
+	res.json(result);
 });
 
-app.delete("/event/:id", async (req: Request, res: Response) => {
-  const reuslt = await handleDeleteOneEvent(req, res)
-  res.send();
+app.delete("/event/:id",auth, async (req: Request, res: Response) => {
+	const reuslt = await handleDeleteOneEvent(req, res);
+	res.send();
 });
 
-app.get("/all-events/info", async (req: Request, res: Response) => {
-  const result = await handleGetAllEventsInfo(req, res);
-  res.json(result);
+app.get("/all-events/info", auth, async (req: Request, res: Response) => {
+	const result = await handleGetAllEventsInfo(req, res);
+	res.json(result);
 });
 
 app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
+	console.log(`[server]: Server is running at http://localhost:${port}`);
 });
