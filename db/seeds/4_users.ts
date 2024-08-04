72b7506a7e2d5f5ba34a064b07b6c923f2462c64
Filippo Quattrocchi <filippo.quattrocchi23@outlook.it>
2024-08-04 16:08:24 +0900

    Fix: Change saltrounds on seed to defaul of env var

--- db/seeds/4_users.ts
+++ db/seeds/4_users.ts
@@ -4,6 +4,7 @@ import bcrypt from "bcrypt";
 export async function seed(knex: Knex): Promise<void> {
 	//const hashPassword = await bcrypt.hash(userCredential.password, saltRounds);
 	// Deletes ALL existing entries
+	const saltRounds = Number(process.env.SALT || 8);
 	await knex("user").del();
 
 	// Inserts seed entries
@@ -11,19 +12,19 @@ export async function seed(knex: Knex): Promise<void> {
 		{
 			id: 100,
 			username: "user",
-			password: await bcrypt.hash("password", 10), //password
+			password: await bcrypt.hash("password", saltRounds), //password
 			email: "email@email.com",
 		},
 		{
 			id: 101,
 			username: "friendly",
-			password: await bcrypt.hash("qwerty", 10),
+			password: await bcrypt.hash("qwerty", saltRounds),
 			email: "ciao@email.com",
 		},
 		{
 			id: 102,
 			username: "condition",
-			password: await bcrypt.hash("asdf", 10),
+			password: await bcrypt.hash("asdf", saltRounds),
 			email: "condition@email.com",
 		},
 	]);
